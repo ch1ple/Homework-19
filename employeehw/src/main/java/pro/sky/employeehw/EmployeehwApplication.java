87e8cc5130ef6905c5673cc06f05868076ff2e50
ch1ple <teavis@mail.ru>
2023-04-11 16:55:43 +0300

    Homework 19

---
+++ employeehw/src/main/java/pro/sky/employeehw/EmployeehwApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.employeehw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeehwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeehwApplication.class, args);
+	}
+
+}
